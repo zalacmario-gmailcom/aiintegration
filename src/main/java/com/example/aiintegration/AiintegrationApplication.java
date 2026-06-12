749a081c8126510b01a33ab740b7bbddcaa75e0d
Mario <zalac.mario1@gmail.com>
2026-06-12 09:50:52 +0200

    first commit

---
+++ src/main/java/com/example/aiintegration/AiintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aiintegration/AiintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
